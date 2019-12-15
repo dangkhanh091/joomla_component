cb618d95005b32a749c49cf47ba98474d4049bcf
Dang Van Khanh <khanhdv@tandan.com.vn>
2019-12-15 23:26:26 +0700

    tạo mới component

---
+++ admin/newcomponent.php
@@ -0,0 +1,10 @@
+Hướng dẫn tạo, cài đặt Component trong Admin !
+<br>
+<br>
+<form>
+Nhập Tiêu Đề:<br>
+<input type="text" name="txtTieuDe" value="Tiêu đề Demo"><br>
+Nhập Nội Dung :<br>
+<input type="text" name="txtNoiDung" value="Nội dung Demo"><br>
+<input type="submit" value="Gửi" onclick="alert('Đã Gửi !');">
+</form>
---
+++ site/newcomponent.php
@@ -0,0 +1,1 @@
+Hướng dẫn tạo và cài đặt Component !
